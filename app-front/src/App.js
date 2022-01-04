b2e082a2106dda01f3c4aaf163f08d408f5937eb
梅存兵 <meicunbing@gmail.com>
2022-01-04 11:30:58 +0800

    界面初始化

--- app-front/src/App.js
+++ app-front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {Admin, Resource, Layout} from 'react-admin';
+import {UserList} from "./users";
+import {PostCreate, PostEdit, PostList} from "./posts";
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from "./admin/Dashboard";
+import authProvider from "./admin/authProvider";
+import dataProvider from "./admin/dataProvider";
+import NotFound from "./error/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// https://marmelab.com/react-admin/Admin.html
+const App = () => (
+    <Admin title="OminLink 全链通，打通服务型企业销售、服务、客户关系环节" dashboard={Dashboard} authProvider={authProvider}
+           dataProvider={dataProvider} disableTelemetry catchAll={NotFound}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
+    </Admin>
+)
 
 export default App;
